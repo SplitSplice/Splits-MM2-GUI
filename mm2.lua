6f9452f3e581fdc8b3b0d43656bff2c1f8c8314b
Tragidal <67218546+Tragidal@users.noreply.github.com>
2024-06-23 18:28:36 -0500

    Create mm2.lua

---
+++ mm2.lua
@@ -0,0 +1,402 @@
+local Instances = {
+	SplitsGUI = Instance.new("ScreenGui"),
+	Frame = Instance.new("Frame"),
+	name1 = Instance.new("TextLabel"),
+	name2 = Instance.new("TextLabel"),
+	Gun = Instance.new("TextButton"),
+	UICorner = Instance.new("UICorner"),
+	UICorner_2 = Instance.new("UICorner"),
+	Knife = Instance.new("TextButton"),
+	UICorner_3 = Instance.new("UICorner"),
+	name1_2 = Instance.new("TextLabel"),
+	X = Instance.new("TextButton"),
+	UICorner_4 = Instance.new("UICorner"),
+	open = Instance.new("TextButton"),
+	UICorner_5 = Instance.new("UICorner"),
+}
+
+Instances.SplitsGUI.Name = "SplitsGUI"
+Instances.SplitsGUI.Parent = game.StarterGui
+
+Instances.Frame.Name = "Frame"
+Instances.Frame.Parent = Instances.SplitsGUI
+Instances.Frame.Size = UDim2.new(0, 448, 0, 490)
+Instances.Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Instances.Frame.BackgroundTransparency = 0.30000001192092896
+Instances.Frame.Position = UDim2.new(0.24860023, 0, 0.169811323, 0)
+Instances.Frame.BorderSizePixel = 0
+Instances.Frame.BackgroundColor3 = Color3.fromRGB(18, 18, 18)
+
+Instances.name1.Name = "name1"
+Instances.name1.Parent = Instances.Frame
+Instances.name1.Size = UDim2.new(0, 166, 0, 34)
+Instances.name1.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Instances.name1.BackgroundTransparency = 1
+Instances.name1.Position = UDim2.new(0.015625, 0, 0.0142857144, 0)
+Instances.name1.BorderSizePixel = 0
+Instances.name1.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Instances.name1.TextColor3 = Color3.fromRGB(255, 255, 255)
+Instances.name1.Text = "Split's MM2 Clone"
+Instances.name1.TextWrapped = true
+Instances.name1.TextSize = 14
+Instances.name1.FontFace = Font.new("rbxasset://fonts/families/FredokaOne.json", Enum.FontWeight.Regular, Enum.FontStyle.Normal)
+Instances.name1.TextScaled = true
+
+Instances.name2.Name = "name2"
+Instances.name2.Parent = Instances.Frame
+Instances.name2.Size = UDim2.new(0, 57, 0, 21)
+Instances.name2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Instances.name2.BackgroundTransparency = 1
+Instances.name2.Position = UDim2.new(0.348214298, 0, 0.0142857144, 0)
+Instances.name2.BorderSizePixel = 0
+Instances.name2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Instances.name2.TextColor3 = Color3.fromRGB(255, 255, 255)
+Instances.name2.Text = "TROLLING GUI!"
+Instances.name2.TextWrapped = true
+Instances.name2.TextSize = 14
+Instances.name2.FontFace = Font.new("rbxasset://fonts/families/FredokaOne.json", Enum.FontWeight.Regular, Enum.FontStyle.Normal)
+Instances.name2.TextScaled = true
+
+Instances.Gun.Name = "Gun"
+Instances.Gun.Parent = Instances.Frame
+Instances.Gun.Size = UDim2.new(0, 200, 0, 50)
+Instances.Gun.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Instances.Gun.BackgroundTransparency = 0.5
+Instances.Gun.Position = UDim2.new(0.0290178563, 0, 0.226530612, 0)
+Instances.Gun.BorderSizePixel = 0
+Instances.Gun.BackgroundColor3 = Color3.fromRGB(18, 18, 18)
+Instances.Gun.TextColor3 = Color3.fromRGB(255, 255, 255)
+Instances.Gun.Text = "Give Gun (FE)"
+Instances.Gun.TextWrapped = true
+Instances.Gun.TextSize = 14
+Instances.Gun.FontFace = Font.new("rbxasset://fonts/families/FredokaOne.json", Enum.FontWeight.Regular, Enum.FontStyle.Normal)
+Instances.Gun.TextScaled = true
+
+Instances.UICorner.Name = "UICorner"
+Instances.UICorner.Parent = Instances.Gun
+Instances.UICorner.CornerRadius = UDim.new(0, 15)
+
+Instances.UICorner_2.Name = "UICorner"
+Instances.UICorner_2.Parent = Instances.Frame
+Instances.UICorner_2.CornerRadius = UDim.new(0, 5)
+
+Instances.Knife.Name = "Knife"
+Instances.Knife.Parent = Instances.Frame
+Instances.Knife.Size = UDim2.new(0, 200, 0, 50)
+Instances.Knife.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Instances.Knife.BackgroundTransparency = 0.5
+Instances.Knife.Position = UDim2.new(0.524553597, 0, 0.226530612, 0)
+Instances.Knife.BorderSizePixel = 0
+Instances.Knife.BackgroundColor3 = Color3.fromRGB(18, 18, 18)
+Instances.Knife.TextColor3 = Color3.fromRGB(255, 255, 255)
+Instances.Knife.Text = "Give Knife (FE)"
+Instances.Knife.TextWrapped = true
+Instances.Knife.TextSize = 14
+Instances.Knife.FontFace = Font.new("rbxasset://fonts/families/FredokaOne.json", Enum.FontWeight.Regular, Enum.FontStyle.Normal)
+Instances.Knife.TextScaled = true
+
+Instances.UICorner_3.Name = "UICorner"
+Instances.UICorner_3.Parent = Instances.Knife
+Instances.UICorner_3.CornerRadius = UDim.new(0, 15)
+
+Instances.name1_2.Name = "name1"
+Instances.name1_2.Parent = Instances.Frame
+Instances.name1_2.Size = UDim2.new(0, 282, 0, 253)
+Instances.name1_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Instances.name1_2.BackgroundTransparency = 1
+Instances.name1_2.Position = UDim2.new(0.185267851, 0, 0.404081643, 0)
+Instances.name1_2.BorderSizePixel = 0
+Instances.name1_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Instances.name1_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+Instances.name1_2.Text = "This is all we have sadly. More may be coming soon, depending on vulnerabilities in the games."
+Instances.name1_2.TextWrapped = true
+Instances.name1_2.TextSize = 14
+Instances.name1_2.FontFace = Font.new("rbxasset://fonts/families/FredokaOne.json", Enum.FontWeight.Regular, Enum.FontStyle.Normal)
+Instances.name1_2.TextScaled = true
+
+Instances.X.Name = "X"
+Instances.X.Parent = Instances.Frame
+Instances.X.Size = UDim2.new(0, 50, 0, 50)
+Instances.X.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Instances.X.BackgroundTransparency = 0.5
+Instances.X.Position = UDim2.new(0.888392866, 0, -0.00204081624, 0)
+Instances.X.BorderSizePixel = 0
+Instances.X.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+Instances.X.TextColor3 = Color3.fromRGB(255, 255, 255)
+Instances.X.Text = "X"
+Instances.X.TextWrapped = true
+Instances.X.TextSize = 14
+Instances.X.FontFace = Font.new("rbxasset://fonts/families/FredokaOne.json", Enum.FontWeight.Regular, Enum.FontStyle.Normal)
+Instances.X.TextScaled = true
+
+Instances.UICorner_4.Name = "UICorner"
+Instances.UICorner_4.Parent = Instances.X
+Instances.UICorner_4.CornerRadius = UDim.new(0, 5)
+
+Instances.open.Name = "open"
+Instances.open.Parent = Instances.SplitsGUI
+Instances.open.Size = UDim2.new(0, 200, 0, 50)
+Instances.open.Visible = false
+Instances.open.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Instances.open.BackgroundTransparency = 0.5
+Instances.open.Position = UDim2.new(0.387935489, 0, 0.931382358, 0)
+Instances.open.BorderSizePixel = 0
+Instances.open.BackgroundColor3 = Color3.fromRGB(18, 18, 18)
+Instances.open.TextColor3 = Color3.fromRGB(255, 255, 255)
+Instances.open.Text = "Open GUI"
+Instances.open.TextWrapped = true
+Instances.open.TextSize = 14
+Instances.open.FontFace = Font.new("rbxasset://fonts/families/FredokaOne.json", Enum.FontWeight.Regular, Enum.FontStyle.Normal)
+Instances.open.TextScaled = true
+
+Instances.UICorner_5.Name = "UICorner"
+Instances.UICorner_5.Parent = Instances.open
+Instances.UICorner_5.CornerRadius = UDim.new(0, 15)
+
+-- Function "LocalScript"
+(function()
+	local script = Instance.new("LocalScript", Instances.Gun)
+	script.Parent.MouseButton1Click:Connect(function()
+		game:GetService("ReplicatedStorage").GiveGun:FireServer()
+		print("Gave the Gun!")
+	end)
+end)()
+
+-- Function "LocalScript"
+(function()
+	local script = Instance.new("LocalScript", Instances.Knife)
+	script.Parent.MouseButton1Click:Connect(function()
+		game:GetService("ReplicatedStorage").GiveKnife:FireServer()
+		print("Gave the Knife!")
+	end)
+end)()
+
+-- Function "LocalScript"
+(function()
+	local script = Instance.new("LocalScript", Instances.X)
+	local UIframe = script.Parent.Parent
+	
+	UIframe.X.MouseButton1Click:Connect(function()
+		UIframe.Visible = false
+		UIframe.Parent.open.Visible = true
+	end)
+end)()
+
+-- Function "BlurController"
+(function()
+	local script = Instance.new("LocalScript", Instances.Frame)
+	local Lighting          = game:GetService("Lighting")
+	local camera			= workspace.CurrentCamera
+	
+	local BLUR_SIZE         = Vector2.new(10, 10)
+	local PART_SIZE         = 0.01
+	local PART_TRANSPARENCY = 1 - 1e-7
+	local START_INTENSITY	= 0.25
+	
+	script.Parent:SetAttribute("BlurIntensity", START_INTENSITY)
+	
+	local BLUR_OBJ          = Instance.new("DepthOfFieldEffect")
+	BLUR_OBJ.FarIntensity   = 0
+	BLUR_OBJ.NearIntensity  = script.Parent:GetAttribute("BlurIntensity")
+	BLUR_OBJ.FocusDistance  = 0.25
+	BLUR_OBJ.InFocusRadius  = 0
+	BLUR_OBJ.Parent         = Lighting
+	
+	local PartsList         = {}
+	local BlursList         = {}
+	local BlurObjects       = {}
+	local BlurredGui        = {}
+	
+	BlurredGui.__index      = BlurredGui
+	
+	function rayPlaneIntersect(planePos, planeNormal, rayOrigin, rayDirection)
+		local n = planeNormal
+		local d = rayDirection
+		local v = rayOrigin - planePos
+	
+		local num = n.x*v.x + n.y*v.y + n.z*v.z
+		local den = n.x*d.x + n.y*d.y + n.z*d.z
+		local a = -num / den
+	
+		return rayOrigin + a * rayDirection, a
+	end
+	
+	function rebuildPartsList()
+		PartsList = {}
+		BlursList = {}
+		for blurObj, part in pairs(BlurObjects) do
+			table.insert(PartsList, part)
+			table.insert(BlursList, blurObj)
+		end
+	end
+	
+	function BlurredGui.new(frame, shape)
+		local blurPart        = Instance.new("Part")
+		blurPart.Size         = Vector3.new(1, 1, 1) * 0.01
+		blurPart.Anchored     = true
+		blurPart.CanCollide   = false
+		blurPart.CanTouch     = false
+		blurPart.Material     = Enum.Material.Glass
+		blurPart.Transparency = PART_TRANSPARENCY
+		blurPart.Parent       = workspace.CurrentCamera
+	
+		local mesh
+		if (shape == "Rectangle") then
+			mesh        = Instance.new("BlockMesh")
+			mesh.Parent = blurPart
+		elseif (shape == "Oval") then
+			mesh          = Instance.new("SpecialMesh")
+			mesh.MeshType = Enum.MeshType.Sphere
+			mesh.Parent   = blurPart
+		end
+		
+		local ignoreInset = false
+		local currentObj  = frame
+		
+		while true do
+			currentObj = currentObj.Parent
+	
+			if (currentObj and currentObj:IsA("ScreenGui")) then
+				ignoreInset = currentObj.IgnoreGuiInset
+				break
+			elseif (currentObj == nil) then
+				break
+			end
+		end
+	
+		local new = setmetatable({
+			Frame          = frame;
+			Part           = blurPart;
+			Mesh           = mesh;
+			IgnoreGuiInset = ignoreInset;
+		}, BlurredGui)
+	
+		BlurObjects[new] = blurPart
+		rebuildPartsList()
+	
+		game:GetService("RunService"):BindToRenderStep("...", Enum.RenderPriority.Camera.Value + 1, function()
+			blurPart.CFrame = camera.CFrame * CFrame.new(0,0,0)
+			BlurredGui.updateAll()
+		end)
+		return new
+	end
+	
+	function updateGui(blurObj)
+		if (not blurObj.Frame.Visible) then
+			blurObj.Part.Transparency = 1
+			return
+		end
+		
+		local camera = workspace.CurrentCamera
+		local frame  = blurObj.Frame
+		local part   = blurObj.Part
+		local mesh   = blurObj.Mesh
+		
+		part.Transparency = PART_TRANSPARENCY
+		
+		local corner0 = frame.AbsolutePosition + BLUR_SIZE
+		local corner1 = corner0 + frame.AbsoluteSize - BLUR_SIZE*2
+		local ray0, ray1
+	
+		if (blurObj.IgnoreGuiInset) then
+			ray0 = camera:ViewportPointToRay(corner0.X, corner0.Y, 1)
+			ray1 = camera:ViewportPointToRay(corner1.X, corner1.Y, 1)
+		else
+			ray0 = camera:ScreenPointToRay(corner0.X, corner0.Y, 1)
+			ray1 = camera:ScreenPointToRay(corner1.X, corner1.Y, 1)
+		end
+	
+		local planeOrigin = camera.CFrame.Position + camera.CFrame.LookVector * (0.05 - camera.NearPlaneZ)
+		local planeNormal = camera.CFrame.LookVector
+		local pos0 = rayPlaneIntersect(planeOrigin, planeNormal, ray0.Origin, ray0.Direction)
+		local pos1 = rayPlaneIntersect(planeOrigin, planeNormal, ray1.Origin, ray1.Direction)
+	
+		local pos0 = camera.CFrame:PointToObjectSpace(pos0)
+		local pos1 = camera.CFrame:PointToObjectSpace(pos1)
+	
+		local size   = pos1 - pos0
+		local center = (pos0 + pos1)/2
+	
+		mesh.Offset = center
+		mesh.Scale  = size / PART_SIZE
+	end
+	
+	function BlurredGui.updateAll()
+		BLUR_OBJ.NearIntensity = tonumber(script.Parent:GetAttribute("BlurIntensity"))
+		
+		for i = 1, #BlursList do
+			updateGui(BlursList[i])
+		end
+	
+		local cframes = table.create(#BlursList, workspace.CurrentCamera.CFrame)
+		workspace:BulkMoveTo(PartsList, cframes, Enum.BulkMoveMode.FireCFrameChanged)
+	
+		BLUR_OBJ.FocusDistance = 0.25 - camera.NearPlaneZ
+	end
+	
+	function BlurredGui:Destroy()
+		self.Part:Destroy()
+		BlurObjects[self] = nil
+		rebuildPartsList()
+	end
+	
+	BlurredGui.new(script.Parent, "Rectangle")
+	
+	BlurredGui.updateAll()
+	return BlurredGui
+end)()
+
+-- Function "UIDrag"
+(function()
+	local script = Instance.new("LocalScript", Instances.Frame)
+	-- Made by Real_IceyDev (@lceyDex) --
+	-- Simple UI dragger (PC Only/Any device that has a mouse) --
+	
+	local UIS = game:GetService('UserInputService')
+	local frame = script.Parent
+	local dragToggle = nil
+	local dragSpeed = 0.25
+	local dragStart = nil
+	local startPos = nil
+	
+	local function updateInput(input)
+		local delta = input.Position - dragStart
+		local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
+			startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+		game:GetService('TweenService'):Create(frame, TweenInfo.new(dragSpeed), {Position = position}):Play()
+	end
+	
+	frame.InputBegan:Connect(function(input)
+		if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then 
+			dragToggle = true
+			dragStart = input.Position
+			startPos = frame.Position
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragToggle = false
+				end
+			end)
+		end
+	end)
+	
+	UIS.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			if dragToggle then
+				updateInput(input)
+			end
+		end
+	end)
+end)()
+
+-- Function "LocalScript"
+(function()
+	local script = Instance.new("LocalScript", Instances.open)
+	local UIframe = script.Parent.Parent.Frame
+	
+	UIframe.X.MouseButton1Click:Connect(function()
+		UIframe.Visible = true
+		UIframe.Parent.open.Visible = false
+	end)
+end)()
+
+--[[ End of generation. ]]
